32063dd30809a95d19013eeba4a3568a697f5349
VladimirVarosyan <113124512+VladimirVarosyan@users.noreply.github.com>
2022-09-08 20:14:43 +0400

    changes

---
+++ Game_Of-Life-prog2/class.js
@@ -0,0 +1,517 @@
+class People {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.multiply = 0;
+
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1    ],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y    ],
+            [this.x + 1, this.y    ],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1    ],
+            [this.x + 1, this.y + 1],
+        ]
+    }
+
+
+    chooseCell(char) {
+        var found = [];
+
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == char) {
+                    found.push(this.directions[i])
+                }
+            }
+
+        }
+        return found;
+    }
+
+
+      mul(){
+            this.multiply++
+             var emptyCell  =  this.chooseCell(0);
+             var newCell  =    random(emptyCell);
+
+                   if(newCell && this.multiply >= 5){
+                          
+                            var newX = newCell[0];
+                            var newY = newCell[1];
+
+                                matrix[newY][newX] = 1;
+
+                                var pe = new People(newX,newY); 
+
+                                peopleArr.push(pe);
+
+                                this.multiply = 0;
+                         
+                   }
+      }
+}
+
+
+class Zombi {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.energy = 10;
+        this.multiply = 0
+        this.directions = [];
+    }
+
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+
+    chooseCell(char) {
+        this.getNewCoordinates()
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+
+                if (matrix[y][x] == char) {
+                    found.push(this.directions[i]);
+                }
+            }
+        }
+        return found;
+    }
+
+    mul() {
+        this.multiply++;
+        var emptyCells = this.chooseCell(0);
+        var newCell = random(emptyCells);
+
+        console.log(emptyCells);
+        if (newCell && this.multiply >= 15) {
+            var newX = newCell[0];  
+            var newY = newCell[1];
+            matrix[newY][newX] = 2;
+
+            var zombi = new Zombi(newX, newY);
+            zombiArr.push(zombi);
+            this.multiply = 0;
+        }
+    }
+
+    move() {
+        this.energy--
+        var emptyCell = this.chooseCell(0)
+        var newCell = random(emptyCell)
+
+        if (newCell && this.energy >= 0) {
+            console.log(newCell)
+            var newX = newCell[0]
+            var newY = newCell[1]
+            matrix[newY][newX] = matrix[this.y][this.x]
+            matrix[this.y][this.x] = 0
+            this.x = newX
+            this.y = newY
+        }
+        else {
+            if (this.energy < 0) {
+                this.die()
+            }
+        }
+    }
+
+    eat() {
+        var emptyCell = this.chooseCell(1)
+        var newCell = random(emptyCell)
+
+        if (newCell) {
+            this.energy++
+            var newX = newCell[0]
+            var newY = newCell[1]
+
+            matrix[newY][newX] = matrix[this.y][this.x]
+            matrix[this.y][this.x] = 0
+            this.x = newX
+            this.y = newY
+            for (var i in peopleArr) {
+                if (newX == peopleArr[i].x && newY == peopleArr[i].y) {
+                    peopleArr.splice(i, 1)
+                    break
+                }
+            }
+        }
+        else {
+            this.move()
+        }
+    }
+
+    die() {
+        matrix[this.y][this.x] = 0;
+        for (var i in zombiArr) {
+            if (this.x == zombiArr[i].x && this.y == zombiArr[i].y) {
+                zombiArr.splice(i, 1);
+                break;
+            }
+        }
+    }
+}
+
+
+class Antivirus {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.energy = 10;
+        this.multiply = 0
+        this.directions = [];
+    }
+
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+
+    chooseCell(character) {
+        this.getNewCoordinates()
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+
+                if (matrix[y][x] == character) {
+                    found.push(this.directions[i]);
+                }
+            }
+        }
+        return found;
+    }
+
+    mul() {
+        this.multiply++;
+        var emptyCells = this.chooseCell(0);
+        var newCell = random(emptyCells);
+
+        console.log(emptyCells);
+        if (newCell && this.multiply >= 15) {
+            var newX = newCell[0];
+            var newY = newCell[1];
+            matrix[newY][newX] = 3;
+
+            var antivirus = new Antivirus(newX, newY);
+            antivirusArr.push(antivirus);
+            this.multiply = 0;
+        }
+    }
+
+    move() {
+        this.energy--
+        var emptyCells = this.chooseCell(0)
+        var newCell = random(emptyCells);
+
+        if (newCell && this.energy >= 0) {
+            console.log(newCell)
+            var newX = newCell[0]
+            var newY = newCell[1]
+            matrix[newY][newX] = matrix[this.y][this.x]
+            matrix[this.y][this.x] = 0
+            this.x = newX
+            this.y = newY
+        }
+        else {
+            if (this.energy < 0) {
+                this.die()
+            }
+        }
+    }
+
+    eat() {
+        var emptyCells = this.chooseCell(1, 2)
+        var newCell = random(emptyCells);
+
+        if (newCell) {
+            this.energy++
+            var newX = newCell[0]
+            var newY = newCell[1]
+
+            matrix[newY][newX] = matrix[this.y][this.x]
+            matrix[this.y][this.x] = 0
+            this.x = newX
+            this.y = newY
+            for (var i in zombiArr) {
+                if (zombiArr[i].x && newY == zombiArr[i].y) {
+                    zombiArr.splice(i, 1)
+                    break
+                }
+            }
+        }
+        else {
+            this.move()
+        }
+    }
+
+    die() {
+        matrix[this.y][this.x] = 0;
+        for (var i in antivirusArr) {
+            if (this.x == antivirusArr[i].x && this.y == antivirusArr[i].y) {
+                antivirusArr.splice(i, 1);
+                break;
+            }
+        }
+    }
+}
+
+
+
+
+class Omnivorous{
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.energy = 10;
+        this.multiply = 0
+        this.directions = [];
+    }
+
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+
+    chooseCell(character) {
+        this.getNewCoordinates()
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+
+                if (matrix[y][x] == character) {
+                    found.push(this.directions[i]);
+                }
+            }
+        }
+        return found;
+    }
+
+    mul() {
+        this.multiply++;
+        var emptyCells = this.chooseCell(0);
+        var newCell = random(emptyCells);
+
+        console.log(emptyCells);
+        if (newCell && this.multiply >= 15) {
+            var newX = newCell[0];
+            var newY = newCell[1];
+            matrix[newY][newX] = 4;
+
+            var omnivorous= new Omnivorous (newX, newY);
+            omnivorousArr.push(omnivorous);
+            this.multiply = 0;
+        }
+    }
+
+    move() {
+        this.energy--
+        var emptyCells = this.chooseCell(0)
+        var newCell = random(emptyCells);
+
+        if (newCell && this.energy >= 0) {
+            console.log(newCell)
+            var newX = newCell[0]
+            var newY = newCell[1]
+            matrix[newY][newX] = matrix[this.y][this.x]
+            matrix[this.y][this.x] = 0
+            this.x = newX
+            this.y = newY
+        }
+        else {
+            if (this.energy < 0) {
+                this.die()
+            }
+        }
+    }
+
+    eat() {
+        var emptyCells = this.chooseCell(1, 2, 3)
+        var newCell = random(emptyCells);
+
+        if (newCell) {
+            this.energy++
+            var newX = newCell[0]
+            var newY = newCell[1]
+
+            matrix[newY][newX] = matrix[this.y][this.x]
+            matrix[this.y][this.x] = 0
+            this.x = newX
+            this.y = newY
+            for (var i in antivirusArr) {
+                if (newX == antivirusArr[i].x && newY == antivirusArr[i].y) {
+                    antivirusArr.splice(i, 1)
+                    break
+                }
+            }
+        }
+        else {
+            this.move()
+        }
+    }
+
+    die() {
+        matrix[this.y][this.x] = 0;
+        for (var i in omnivorousArr) {
+            if (this.x == omnivorousArr[i].x && this.y ==  omnivorousArr[i].y) {
+                omnivorousArr.splice(i, 1)
+
+                break;
+            }
+        }
+    }
+}
+
+
+
+
+
+class Coronavirus{
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.energy = 10;
+        this.multiply = 0
+        this.directions = [];
+    }
+
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+
+    chooseCell(character) {
+        this.getNewCoordinates()
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+
+                if (matrix[y][x] == character) {
+                    found.push(this.directions[i]);
+                }
+            }
+        }
+        return found;
+    }
+
+    mul() {
+        this.multiply++;
+        var emptyCells = this.chooseCell(0);
+        var newCell = random(emptyCells);
+
+        console.log(emptyCells);
+        if (newCell && this.multiply >= 15) {
+            var newX = newCell[0];
+            var newY = newCell[1];
+            matrix[newY][newX] = 5;
+
+            var coronavirus= new Coronavirus (newX, newY);
+            coronavirusArr.push(coronavirus);
+            this.multiply = 0;
+        }
+    }
+
+    move() {
+        this.energy--
+        var emptyCells = this.chooseCell(0)
+        var newCell = random(emptyCells);
+
+        if (newCell && this.energy >= 0) {
+            console.log(newCell)
+            var newX = newCell[0]
+            var newY = newCell[1]
+            matrix[newY][newX] = matrix[this.y][this.x]
+            matrix[this.y][this.x] = 0 
+            this.x = newX
+            this.y = newY
+        }
+        else {
+            if (this.energy < 0) {
+                this.die()
+            }
+        }
+    }
+
+    eat() {
+        var emptyCells = this.chooseCell(1, 2, 3, 4)
+        var newCell = random(emptyCells);
+
+        if (newCell) {
+            this.energy++
+            var newX = newCell[0]
+            var newY = newCell[1]
+
+            matrix[newY][newX] = matrix[this.y][this.x]
+            matrix[this.y][this.x] = 0
+            this.x = newX
+            this.y = newY
+            for (var i in omnivorousArr) {
+                if (newX == omnivorousArr[i].x && newY == omnivorousArr[i].y) {
+                    omnivorousArr.splice(i, 1)
+                    break
+                }
+            }
+        }
+        else {
+            this.move()
+        }
+    }
+
+    die() {
+        matrix[this.y][this.x] = 0;
+        for (var i in coronavirusArr) {
+            if (this.x == coronavirusArr[i].x && this.y ==  coronavirusArr[i].y) {
+                coronavirusArr.splice(i, 1)
+
+                break;
+            }
+        }
+    }
+}
